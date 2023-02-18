43618f9e07843ced5101b5639c1d8656f3b1ae43
Ekaterina Driga <kate.driga@yandex.ru>
2023-02-18 18:41:41 +0300

    fix: debounce (remove this)

--- src/utils/util-functions/debounce.ts
+++ src/utils/util-functions/debounce.ts
@@ -1,10 +1,10 @@
 export function debounce(func: (...args) => void, timeoutMs = 500) {
-  return function (...args) {
-    const previousCall: number | undefined = this.lastCall;
-    this.lastCall = Date.now();
-    if (previousCall && this.lastCall - previousCall <= timeoutMs) {
-      clearTimeout(this.lastCallTimer);
-    }
-    this.lastCallTimer = setTimeout(() => func(...args), timeoutMs);
+  let id = null;
+  return (...args) => {
+    clearTimeout(id);
+    id = setTimeout(() => {
+      func(...args);
+      clearTimeout(id);
+    }, timeoutMs);
   };
 }
