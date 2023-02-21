b32f8122467ec61537c362856a2c6f8e4474b1b9
Ekaterina Driga <kate.driga@yandex.ru>
2023-02-21 20:37:13 +0300

    chore: test navigation removed

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -2,14 +2,13 @@ import styles from './header.module.scss';
 import { Block } from '../../utils/base-components/block';
 import { TVirtualDomNode } from '../../utils/template/template-types';
 import { Template } from '../../utils/template/template';
-import { Navigation } from '../navigation/navigation';
 
 export class Header extends Block<null, null> {
   render(): TVirtualDomNode {
     return Template.createElement(
-      'nav',
-      { key: 'nav', class: styles.header },
-      Template.createComponent(Navigation, { key: 'nav' })
+      'h1',
+      { key: 'header', class: styles.header },
+      Template.createTextElement('Welcome to Chat')
     );
   }
 }
